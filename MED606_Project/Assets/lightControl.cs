b2795a0033d4f04fd48e0b5b5b40980fdabc983b
malteerasmussen <43847716+malteerasmussen@users.noreply.github.com>
2021-05-02 12:57:40 +0200

    light stuff malte and tor

---
+++ MED606_Project/Assets/lightControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IA
+{
+    
+    public class lightControl : MonoBehaviour
+    {
+        ArtNetData artNetData;
+
+        [Range(0, 255)]
+        public int intensity;
+
+
+        
+    }
+
+}
